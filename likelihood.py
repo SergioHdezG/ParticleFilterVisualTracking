a069bd4d3adac3a23030192a505c6e8084c954c0
SergioHdezG <sergio.hernandez@urjc.es>
2022-05-31 10:26:00 +0200

    Initial commit

---
+++ likelihood.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 13 17:14:50 2022
+
+@author: ErnestoAngelCurbeloB
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x,a):
+    
+    return np.sin(x*a)
+# def f(x,a):
+    
+#     return x*a
+
+
+real_a = 2
+
+ndata = 4
+
+sig = 0.5
+
+t = np.arange(0,ndata)
+
+y = np.array([f(ti,real_a) + sig*np.random.normal() for ti in t])
+
+# %%%% plot function
+
+tplot = np.arange(0,ndata,0.01)
+
+yplot = [f(ti,real_a) for ti in tplot]
+
+plt.plot(tplot,yplot)
+plt.scatter(t,y)
+
+
+# %% likelihood
+
+def like(a):
+    
+    aux = np.array([f(xi,a) for xi in t])
+    
+    aux2 = np.array(y)
+    
+    exponent = (((aux2 - aux)**2).sum())/(2*sig)
+    
+    down = (2*np.pi*sig)**(len(y)/2)
+    
+    ans = np.exp(-exponent)/down
+    
+    return ans
+    
+
+# %% plot likelihood
+
+a_array = np.arange(-10,10,0.01)
+lplot = np.array([like(a) for a in a_array])
+
+plt.plot(a_array,lplot)
+plt.show()
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
